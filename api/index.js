69768b579159d699c49f4b18eb6193ddd523c386
Max Heichling <max@heichling.xyz>
2022-04-20 15:17:06 +0200

    move to root folder

--- api/index.js
+++
@@ -1,9 +0,0 @@
-const coap = require('coap')
-const server = coap.createServer()
-
-server.on('request', (req, res) => {
-  console.log("Request da");
-  res.end('Hello ' + req.url.split('/')[1] + '\n')
-})
-
-server.listen(3000, () => {console.log("yoooo Server laufen tuen")});
